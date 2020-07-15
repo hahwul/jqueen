a5e31a809c9a9f6607664f28921fdf86f77298ab
hahwul <hahwul@gmail.com>
2020-07-16 01:48:14 +0900

    Update root cmd

--- cmd/root.go
+++ cmd/root.go
@@ -24,21 +24,13 @@ import (
 	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var cfgFile, host string
+var port int
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "jqueen",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "jqueen is simple job queue manager",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -58,10 +50,11 @@ func init() {
 	// will be global for your application.
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.jqueen.yaml)")
+	rootCmd.PersistentFlags().StringVar(&host, "host", "localhost", "daemon mode: binding address / client mode: connect address")
+	rootCmd.PersistentFlags().IntVar(&port, "port", 6886, "daemon mode: listen port / client mode: connect port")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
