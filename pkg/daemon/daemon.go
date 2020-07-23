9a1c20c4eb919302a3b2e5382640f41ba2237fa8
hahwul <hahwul@gmail.com>
2020-07-24 01:31:21 +0900

    Add rest api server

---
+++ pkg/daemon/daemon.go
@@ -0,0 +1,26 @@
+package daemon
+
+import (
+	"net/http"
+	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo"
+	"github.com/hahwul/jqueen/pkg/job"
+)
+
+// RunDaemon is start rest api server
+func RunDaemon() {
+	var queue *JobQueue = New()
+	_= queue
+	e := echo.New()
+	e.Use(middleware.SecureWithConfig(middleware.SecureConfig{
+		XSSProtection:         "",
+		ContentTypeNosniff:    "",
+		XFrameOptions:         "",
+		HSTSMaxAge:            3600,
+		ContentSecurityPolicy: "default-src 'self'",
+	}))
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "jqueen")
+	})
+	e.Logger.Fatal(e.Start(":6886")) 
+}
