f84abaf861804e277f5c1c3a9a163c9be8b615b3
hahwul <hahwul@gmail.com>
2020-07-25 02:00:14 +0900

    Add handler

---
+++ pkg/daemon/handler.go
@@ -0,0 +1,33 @@
+package daemon
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/hahwul/jqueen/pkg/job"
+	"github.com/labstack/echo"
+)
+
+type Return struct {
+	Code int `json:"code"`
+	JobID string `json:"jobid"`
+}
+
+func AddJob(c echo.Context, jq *job.JobQueue) error {
+	// Make JobID
+	t := time.Now()
+	sha256 := sha256.Sum256([]byte(t.String()))
+	s := fmt.Sprintf("%x",sha256)
+
+	j := new(job.Job)
+	j.ID = s	
+
+	jq.Enqueue(j)
+	r := &Return{
+		Code: 0,
+		JobID: j.ID,
+	}
+	return c.JSON(http.StatusOK,r)
+}
